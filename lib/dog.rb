ec7822bfce58d491d1a459b4d981231a9efab6e2
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-06 04:18:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,27 +1,20 @@
 class Dog
-  def initialize(name)
+  def initialize(name, breed = "Mutt")
     @name = name
+    @breed = breed
   end
  
-  def name=(name)
+  def name=(name, breed = "Mutt")
     @name = name
+    @breed = breed
   end
  
   def name
     @name
   end 
   
-  def initialize(breed)
-    @breed = breed
-  end
- 
-  def breed=(breed)
-    @breed = breed
-  end
- 
   def breed
     @breed
   end
 end
 
-Dog.new("Mutt")
